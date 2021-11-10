2d126e780ebbe28247d26ffa9472c7a3da0789f9
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-11-10 07:59:18 +0000

    a bunch of improvements

--- R/diseases.R
+++ R/diseases.R
@@ -10,25 +10,28 @@ X <- diseases %>%
   mutate(rate = count / population * 100000 * 52 / weeks_reporting) %>%
   mutate(state = reorder(state, desc(state)))
 
-colours <- colorRampPalette(c("royalblue", "green", "yellow", "orange", "red"), bias = 3.5)
+colours <- colorRampPalette(c("#F0FFFF", "cyan", "#007FFF", "yellow", "#FFBF00", "orange", "red", "#7F0000"), bias = 2.25)
 
 X %>%
-  filter(disease %in% c("Pertussis", "Measles", "Polio")) %>%
+  filter(disease %in% c("Measles")) %>%
   ggplot(aes(year, state, fill = rate)) +
-  geom_tile(color = "white", size = 0.35) +
-  scale_x_continuous(expand = c(0,0)) +
-  scale_fill_gradientn(colors = colours(20), na.value = 'grey95') +
+  geom_tile(colour = "white", size = 0.5) +
+  geom_vline(xintercept = 1963, col = "black", size = 2, alpha = .5) +
+  scale_x_continuous(breaks = seq(1930, 2010, 10), expand = c(0,0)) +
+  scale_fill_gradientn(colors = colours(16), na.value = 'white') +
+  coord_equal() +
   labs(
     x = "",
     y = "",
-    title = "Battling Measles in the 20th Century",
-    subtitle = "The Impact of Vaccines",
-    caption = ""
+    title = "Measles",
+    subtitle = "Number of cases per 100,000 people.",
+    caption = "Note: CDC data from 2003-2012 comes from its Summary of Notifiable Diseases, which\n publishes yearly rather than weekly and counts confirmed cases as opposed to provisional ones."
   ) +
   theme_ipsum_rc() +
   theme(
-    legend.position = "bottom"
+    legend.position = "bottom",
+    legend.title = element_blank()
   ) +
-  facet_wrap(~disease, ncol = 1, scales = "free_x")
+  annotate(geom = "text", x = 1963, y = 42, label = "Vaccine introduced in 1963", size = 6, hjust = -.1)
 
-ggsave(here("plots", "diseases.png"), bg = "white", height = 9000, width = 3000, units = "px")
+ggsave(here("plots", "diseases.png"), bg = "white", height = 4000, width = 4000, units = "px")
