5346164040607cfd7c354549411b814952b34437
Matthew Henderson <matthew.james.henderson@gmail.com>
2025-02-19 09:40:38 +0000

    Add gun murders plot to targets pipeline.

--- _targets.R
+++ _targets.R
@@ -1,7 +1,7 @@
 library(targets)
 
 tar_option_set(
-  packages = c("readr", "tibble")
+  packages = c("dplyr", "ggplot2", "hrbrthemes", "readr", "tibble")
 )
 
 list(
@@ -12,5 +12,36 @@ list(
   tar_target(
        name = gun_murders,
     command = read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-12-10/gun_murders.csv")
+  ),
+  tar_target(
+       name = gun_murders_plot,
+    command = {
+      gun_murders |>
+	mutate(
+	  country = reorder(country, count)
+	) |>
+	ggplot(aes(country, count)) +
+	geom_point() +
+	labs(
+	     title = "Homicide in the G8",
+	  subtitle = "Rates of homicide in G8 member countries.",
+	   caption = "source: United Nations Office on Drugs and Crime",
+	         x = "",
+	         y = "Gun-related homicides\nper 100,000 people"
+	) +
+	coord_flip() +
+	theme_ipsum_rc()
+    }
+  ),
+  tar_target(
+       name = save_gun_murders_plot,
+    command = ggsave(
+          plot = gun_murders_plot,
+      filename = "plots/gun-murders.png",
+            bg = "white",
+        height = 2000,
+         width = 2000,
+         units = "px"
+    )
   )
 )
